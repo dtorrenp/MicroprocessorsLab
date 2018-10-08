1aae2a6a9711b0cc6953f0ccdefb7cfaaad694c9
dt1716 <dt1716@skbl-422-002.ph.ic.ac.uk>
2018-10-08 11:35:45 +0100

    use port d as an input

--- Simple1.asm
+++ Simple1.asm
@@ -7,13 +7,15 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
+	movlw 	0xFF
+	movwf	TRISD, ACCESS	    ; Port D all inputs
 	movlw 	0x00
 	movwf	TRISC, ACCESS	    ; Port C all outputs
 	bra 	test
 loop	movff 	0x07, PORTC
 	incf 	0x07, W, ACCESS
 test	movwf	0x07, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
+	movf	PORTD, W, ACCESS	    ; use Port D to set length of loop
 	cpfsgt 	0x07, ACCESS
 	bra 	loop		    ; Not yet finished goto start of loop again
 	goto 	0x0		    ; Re-run program from start
