91610336cb3c9dcadbfbb2e7375521fe0a91d59c
dt1716 <dt1716@skbl-422-002.ph.ic.ac.uk>
2018-10-15 11:48:09 +0100

    use different ports

--- Simple1.asm
+++ Simple1.asm
@@ -5,23 +5,22 @@
 	goto	start
 	org 0x100		    ; Main code starts here at address 0x100
 	
-start	movlw 0xFD
+start	movlw 0x00
 	movwf 0x03
-	movlw 0x02
-	movwf 0x04
 	movlw 0x00 
-	movwf TRISF, A		    ; all bits out
-	movwf TRISH, A		    ; all bits out
+	movwf TRISD, A		    ; all bits out on Port F
+	movwf TRISE, A		    ; all bits out on Port H
 	
-increase movff 0x03, PORTF
+increase movff 0x03, PORTD
 	incf  0x03
 	
-clock_on movlw  0x03
-	movwf PORTH
+clock_on movlw  0x01
+	movwf PORTE
 	
-clock_off movlw  0x02
-	movwf PORTH
+clock_off movlw  0x00
+	movwf PORTE
 	
 	bra increase
 	
 	end
+	
