8250eb2a347a29892eb513ee3580c28dce5948de
dt1716 <dt1716@skbl-422-002.ph.ic.ac.uk>
2018-10-15 15:18:17 +0100

    first flip flop

--- Simple1.asm
+++ Simple1.asm
@@ -5,20 +5,31 @@
 	goto	start
 	org 0x100		    ; Main code starts here at address 0x100
 	
-start	movlw 0x00
+start	
+	movlw 0x00
 	movwf 0x03
 	movlw 0x00 
-	movwf TRISD, A		    ; all bits out on Port F
-	movwf TRISE, A		    ; all bits out on Port H
+	movwf TRISD, A		    ; all bits out on Port D
+	movwf TRISE, A		    ; all bits out on Port E
 	
-increase movff 0x03, PORTD
+increase 
+	movlw 0x01
+	movwf TRISE, A
+	movff 0x03, PORTE
 	incf  0x03
 	
-clock_on movlw  0x01
-	movwf PORTE
+ClockOnWrite
+	movlw 0x00
+	movwf TRISE, A
+	movlw 0x01
+	movwf PORTD
 	
-clock_off movlw  0x00
-	movwf PORTE
+clock_offRead 	
+	movlw  0x02
+	movwf PORTD
+	movlw 0xFF
+	movwf TRISE, A
+	movff PORTE,0x04
 	
 	bra increase
 	
