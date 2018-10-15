83bea4355fa395eb5eab1d265945d50525a85f55
dt1716 <dt1716@skbl-422-002.ph.ic.ac.uk>
2018-10-15 16:52:24 +0100

    two flip flops

--- Simple1.asm
+++ Simple1.asm
@@ -6,32 +6,66 @@
 	org 0x100		    ; Main code starts here at address 0x100
 	
 start	
-	movlw 0x00
-	movwf 0x03
+	movlw 0x04
+	movwf 0x05
+	movlw 0x07
+	movwf 0x06
+	
 	movlw 0x00 
 	movwf TRISD, A		    ; all bits out on Port D
 	movwf TRISE, A		    ; all bits out on Port E
+	movwf TRISC, A
+	movwf TRISF, A
 	
 increase 
-	movlw 0x01
-	movwf TRISE, A
-	movff 0x03, PORTE
-	incf  0x03
+	incf  0x05
+	incf  0x06
 	
-ClockOnWrite
-	movlw 0x00
-	movwf TRISE, A
-	movlw 0x01
+ClockOnWrite	
+	movlw 0x00		    ; Enable Port E output
+	movwf TRISE, A	
+	
+	movlw 0x0A		    ; Diable memory outputs
+	movwf PORTD		  	    
+	movff 0x05, PORTE	    ; Set data pattern on Port E
+	movlw 0x0B		    ; Clock in Memory 1
 	movwf PORTD
 	
-clock_offRead 	
-	movlw  0x02
-	movwf PORTD
-	movlw 0xFF
-	movwf TRISE, A
-	movff PORTE,0x04
+	call delayset
 	
+	movlw 0x0A		    ; Diable memory outputs
+	movwf PORTD
+	
+	call delayset
+	
+	movff 0x06, PORTE	    ; Set data pattern on Port E
+	movlw 0x0E		    ; Clock in Memory 2
+	movwf PORTD
+	
+clock_offRead
+	movlw 0xFF		    ; Enable Port E input
+	movwf TRISE, A	
+	
+	movlw  0x08		    ; Enable memory outputs
+	movwf PORTD
+	
+	call delayset
+	
+	movff PORTE,PORTC	    ; Show read data
+	
+	movlw  0x02		    ; Enable memory outputs
+	movwf PORTD
+	
+	call delayset
+	
+	movff PORTE,PORTF	    ; Show read data
+
 	bra increase
 	
+delayset    movlw 0x02
+	movwf 0x09
+delay	decfsz 0x09
+	bra delay
+	return
 	end
 	
