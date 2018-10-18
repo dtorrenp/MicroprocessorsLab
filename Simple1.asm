1eeebcff29b52df7450ec9b56e8b0553be36fe8d
dt1716 <dt1716@skbl-422-002.ph.ic.ac.uk>
2018-10-18 15:01:59 +0100

    working two flip flops, changed wires and followed all high convention

--- Simple1.asm
+++ Simple1.asm
@@ -15,7 +15,10 @@ start
 	movwf TRISD, A		    ; all bits out on Port D
 	movwf TRISE, A		    ; all bits out on Port E
 	movwf TRISC, A
-	movwf TRISF, A
+	movwf TRISJ, A
+	
+	movlw 0x0F		    ; Set all to high
+	movwf PORTD
 	
 increase 
 	incf  0x05
@@ -25,47 +28,44 @@ ClockOnWrite
 	movlw 0x00		    ; Enable Port E output
 	movwf TRISE, A	
 	
-	movlw 0x0A		    ; Diable memory outputs
-	movwf PORTD		  	    
 	movff 0x05, PORTE	    ; Set data pattern on Port E
-	movlw 0x0B		    ; Clock in Memory 1
+	
+	movlw 0x0E		    ; clock one to low
+	movwf PORTD		  	    
+
+	movlw 0x0F		    ; Clock one high
 	movwf PORTD
-	
-	call delayset
-	
-	movlw 0x0A		    ; Diable memory outputs
-	movwf PORTD
-	
-	call delayset
 	
 	movff 0x06, PORTE	    ; Set data pattern on Port E
-	movlw 0x0E		    ; Clock in Memory 2
+	
+	movlw 0x0B		    ; Clock two to low
+	movwf PORTD
+	
+	movlw 0x0F		    ; Clock one to high
 	movwf PORTD
 	
 clock_offRead
 	movlw 0xFF		    ; Enable Port E input
 	movwf TRISE, A	
 	
-	movlw  0x08		    ; Enable memory outputs
+	movlw  0x0D		    ; Move memory one OE low
 	movwf PORTD
-	
-	call delayset
 	
 	movff PORTE,PORTC	    ; Show read data
 	
-	movlw  0x02		    ; Enable memory outputs
+	movlw  0x0F		    ; raise memory one OE
 	movwf PORTD
 	
-	call delayset
+	movlw  0x07		    ; lower memory two OE
+	movwf PORTD
 	
-	movff PORTE,PORTF	    ; Show read data
+	movff PORTE,PORTJ	    ; Show read data
+	
+	movlw  0x0F		    ; raise memory two OE
+	movwf PORTD
 
 	bra increase
+
 	
-delayset    movlw 0x02
-	movwf 0x09
-delay	decfsz 0x09
-	bra delay
-	return
 	end
 	
